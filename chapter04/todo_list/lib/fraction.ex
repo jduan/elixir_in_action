069f1186a1c13c482d5167ecb5552f3a9f026143
Jingjing Duan <jduan@twitter.com>
2015-09-26 17:46:44 -0700

    improve Fraction struct

--- chapter04/todo_list/lib/fraction.ex
+++ chapter04/todo_list/lib/fraction.ex
@@ -3,6 +3,8 @@ defmodule Fraction do
 
   def new(a, b), do: %Fraction{a: a, b: b}
 
+  def value(%Fraction{a: a, b: b}), do: a / b
+
   def add(%Fraction{a: a1, b: b1}, %Fraction{a: a2, b: b2}) do
     new_a = a1 * b2 + a2 * b1
     new_b = b1 * b2
