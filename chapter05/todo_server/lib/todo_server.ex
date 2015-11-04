868c71a06a5f07bc6a1bf39d35dafe1b807b068e
Jingjing Duan <jduan@twitter.com>
2015-11-03 23:16:19 -0800

    implement TodoServer using GenServer

--- chapter05/todo_server/lib/todo_server.ex
+++ chapter05/todo_server/lib/todo_server.ex
@@ -1,46 +1,50 @@
 defmodule TodoServer do
+  use GenServer
   @alias __MODULE__
 
   # public API
   def start do
-    pid = ServerProcess.start(TodoServer)
-    Process.register(pid, @alias)
+    GenServer.start(__MODULE__, nil, name: @alias)
   end
 
   def add_entry(new_entry) do
-    ServerProcess.cast(@alias, {:add_entry, new_entry})
+    GenServer.cast(@alias, {:add_entry, new_entry})
   end
 
   def update_entry(new_entry) do
-    ServerProcess.cast(@alias, {:update_entry, new_entry})
+    GenServer.cast(@alias, {:update_entry, new_entry})
   end
 
   def delete_entry(entry_id) do
-    ServerProcess.cast(@alias, {:delete_entry, entry_id})
+    GenServer.cast(@alias, {:delete_entry, entry_id})
   end
 
   def entries(date) do
-    ServerProcess.call(@alias, {:entries, date})
+    GenServer.call(@alias, {:entries, date})
   end
 
   # implementation detail
-  def init do
-    TodoList.new
+  def init(_) do
+    {:ok, TodoList.new}
   end
 
   def handle_cast({:add_entry, new_entry}, todo_list) do
-    TodoList.add_entry(todo_list, new_entry)
+    {:noreply, TodoList.add_entry(todo_list, new_entry)}
   end
 
   def handle_cast({:update_entry, new_entry}, todo_list) do
-    TodoList.update_entry(todo_list, new_entry)
+    {:noreply, TodoList.update_entry(todo_list, new_entry)}
   end
 
   def handle_cast({:delete_entry, entry_id}, todo_list) do
-    TodoList.delete_entry(todo_list, entry_id)
+    {:noreply, TodoList.delete_entry(todo_list, entry_id)}
   end
 
-  def handle_call({:entries, date}, todo_list) do
-    {TodoList.entries(todo_list, date), todo_list}
+  def handle_call({:entries, date}, _, todo_list) do
+    {:reply, TodoList.entries(todo_list, date), todo_list}
+  end
+
+  def handle_info(_, todo_list) do
+    {:noreply, todo_list}
   end
 end
