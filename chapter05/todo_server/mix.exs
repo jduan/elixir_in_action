3532af3a4775f06e75f3484f69ddf7ac77b081e1
Jingjing Duan <jduan@twitter.com>
2015-12-26 11:25:33 -0800

    add deps on ExDoc

--- chapter05/todo_server/mix.exs
+++ chapter05/todo_server/mix.exs
@@ -39,9 +39,13 @@ defmodule TodoServer.Mixfile do
   # Type "mix help deps" for more examples and options
   defp deps do
     [
-      {:gproc, "0.5.0"},
+      {:earmark, "~> 0.1", only: :dev},
+      {:ex_doc, "~> 0.11", only: :dev},
+
       {:cowboy, "1.0.4"},
+      {:gproc, "0.5.0"},
       {:plug, "1.0.3"},
+
       {:httpoison, "0.8.0", only: :test},
       {:meck, "0.8.3", only: :test},
     ]
